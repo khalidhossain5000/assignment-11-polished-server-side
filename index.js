f6610283671b4655d0643de954ca571f3a5e8c34
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-08-10 11:43:34 +0600

    user collection added and user post api added

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const admin = require("firebase-admin");
 require("dotenv").config();
 
 // app.use(cors());
-app.use(cors({
-  origin:['https://assignment-11-library-cloud-web-app.netlify.app'],
-  credentials:true
-}))
+app.use(
+  cors({
+    origin: [
+      "https://assignment-11-library-cloud-web-app.netlify.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 //DB SETUP START
@@ -29,38 +34,35 @@ const client = new MongoClient(uri, {
 
 //firebase service key related code
 
-const decoded=Buffer.from(process.env.FB_SERVICE_KEY,'base64').toString('utf-8')
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf-8"
+);
 
 // const serviceAccount = require("./assignment-11-firebase-admin-service-key.json");
 const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 //FIREBASE TOKEN RELATED VERIFICATION START
-  const verifyFirebaseToken=async(req,res,next)=>{
-  const authHeader=req.headers?.authorization;
-  if(!authHeader || !authHeader.startsWith('Bearer ')){
-    return res.status(401).send({message:'Unauthorized Access'})
+const verifyFirebaseToken = async (req, res, next) => {
+  const authHeader = req.headers?.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).send({ message: "Unauthorized Access" });
   }
 
-  const token=authHeader.split(' ')[1]
+  const token = authHeader.split(" ")[1];
 
-  try{
-    const decoded=await admin.auth().verifyIdToken(token)
-    req.decoded=decoded
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.decoded = decoded;
     // console.log("from try ",decoded)
-    next()
+    next();
+  } catch (error) {
+    res.status(401).send({ message: "Unauthorised access in catch section" });
   }
-
-  catch(error){
-    res.status(401).send({message:"Unauthorised access in catch section"})
-  }
-
-  
-  
-  }
+};
 //FIREBASE TOKEN RELATED VERIFICATION ENDS
 async function run() {
   try {
@@ -73,45 +75,52 @@ async function run() {
     const borrowBooksCollections = client
       .db("Assignment_11_DB")
       .collection("BorrowBooks");
+      const usersCollection = client
+      .db("Assignment_11_DB")
+      .collection("AllUsers");
 
     // All Books related api start here
-    app.post("/books",verifyFirebaseToken, async (req, res) => {
+    app.post("/books", verifyFirebaseToken, async (req, res) => {
       const allBooks = req.body;
       const result = await allBooksCollections.insertOne(allBooks);
       res.send(result);
     });
     //GETTING ALL BOOKS START HERE
-    app.get("/allBooks",verifyFirebaseToken, async (req, res) => {
+    app.get("/allBooks", async (req, res) => {
       const result = await allBooksCollections.find().toArray();
       res.send(result);
     });
-    app.get('/categoryAllBooks',async (req,res)=>{
+    app.get("/categoryAllBooks", async (req, res) => {
       const result = await allBooksCollections.find().toArray();
       res.send(result);
-    })
-    app.get("/allBooks/:id",verifyFirebaseToken, async (req, res) => {
+    });
+    app.get("/allBooks/:id", verifyFirebaseToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await allBooksCollections.findOne(query);
       res.send(result);
     });
-//GETTING ALL BOOKS ENDS HERE
+    //GETTING ALL BOOKS ENDS HERE
     //BORROW BOOK PAGE RETURN BTN FUNCTIONLITY API--INCREASE BOOKS QUANTITY
-    app.patch("/all-books/quantity/:id",verifyFirebaseToken, async (req, res) => {
-      const id = req.params.id;
-      console.log("from patch",req.decoded)
-      console.log(id)
-      const filter = { _id: new ObjectId(id) };
-      console.log(filter);
-      const updatedDocs = {
-        $inc: {
-          quantity: 1,
-        },
-      };
-      const result = await allBooksCollections.updateOne(filter, updatedDocs);
+    app.patch(
+      "/all-books/quantity/:id",
+      verifyFirebaseToken,
+      async (req, res) => {
+        const id = req.params.id;
+        console.log("from patch", req.decoded);
+        console.log(id);
+        const filter = { _id: new ObjectId(id) };
+        console.log(filter);
+        const updatedDocs = {
+          $inc: {
+            quantity: 1,
+          },
+        };
+        const result = await allBooksCollections.updateOne(filter, updatedDocs);
 
-      res.send(result);
-    });
+        res.send(result);
+      }
+    );
 
     app.put("/all-books/:id", async (req, res) => {
       const id = req.params.id;
@@ -137,7 +146,7 @@ async function run() {
     });
 
     //BORROW BOOK API
-    app.post("/borrow-books/:bookId",verifyFirebaseToken, async (req, res) => {
+    app.post("/borrow-books/:bookId", verifyFirebaseToken, async (req, res) => {
       const id = req.params.bookId;
       const borrowBookData = req.body;
       // console.log("borrowbooks", borrowBookData);
@@ -172,14 +181,12 @@ async function run() {
     });
 
     //GET ALL BORROWED BOOKS BY USER EMIAL
-    app.get("/borrowed-books/:email",verifyFirebaseToken, async (req, res) => {
+    app.get("/borrowed-books/:email", verifyFirebaseToken, async (req, res) => {
       const email = req.params.email;
 
-      if(email !== req.decoded.email){
-        return res.status(403).res.send({message:"FOrbidden access"})
+      if (email !== req.decoded.email) {
+        return res.status(403).res.send({ message: "FOrbidden access" });
       }
-
-
 
       const filter = { userEmail: email };
       const borrowedBooks = await borrowBooksCollections.find(filter).toArray();
@@ -201,12 +208,35 @@ async function run() {
     });
 
     //DELETE BOOK FROM BORROW LIST CLICKING RETURN BTN
-    app.delete("/borrowed-books/:id",verifyFirebaseToken, async (req, res) => {
+    app.delete("/borrowed-books/:id", verifyFirebaseToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await borrowBooksCollections.deleteOne(query);
       res.send(result);
     });
+
+
+    //USER RELATED API START HERE 
+    app.post('/users',async(req,res)=>{
+      console.log(req.body,"helldgsg");
+      const userInfo=req.body;
+      const result=await usersCollection.insertOne(userInfo)
+      res.send(result)
+    })
+   
+    //USER RELATED API ENDS HERE 
+
+
+
+
+
+
+    //BLOGS RELATED API START HERE
+
+    //BLOGS RELATED API ENDS HERE
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
